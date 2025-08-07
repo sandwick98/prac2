97a63b4f55837f20b7edb2799725fb7a2a3c9806
a1969953@Sandaru <a1969953@Sandaru.>
2025-08-07 10:28:15 +0930

    first commit

---
+++ function-1-3.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+// returns number of 0-9s in array
+void count_digits(int array[4][4]){
+    // initialise counters
+    int count_0 = 0;
+    int count_1 = 0;
+    int count_2 = 0;
+    int count_3 = 0;
+    int count_4 = 0;
+    int count_5 = 0;
+    int count_6 = 0;
+    int count_7 = 0;
+    int count_8 = 0;
+    int count_9 = 0;
+
+    // check array for counters
+    for (int i = 0; i < 4; i++){
+        for (int j = 0; j < 4; j++){
+            switch (array[i][j]){
+                case 0: count_0++; break;
+                case 1: count_1++; break;
+                case 2: count_2++; break;
+                case 3: count_3++; break;
+                case 4: count_4++; break;
+                case 5: count_5++; break;
+                case 6: count_6++; break;
+                case 7: count_7++; break;
+                case 8: count_8++; break;
+                case 9: count_9++; break;
+            }
+        }
+    }
+
+    // print results
+    cout << "0:" << count_0 << ";1:" << count_1 << ";2:" << count_2 << ";3:" << count_3 << ";4:" << count_4 << ";5:" << count_5 << ";6:" << count_6 << ";7:" << count_7 << ";8:" << count_8 << ";9:" << count_9 << endl;
+}
